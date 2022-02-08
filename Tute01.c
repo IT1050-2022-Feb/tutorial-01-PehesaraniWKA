c456678658da27ea4f67d66abc64698ecb4c8632
AmaPehesarani <91754460+PehesaraniWKA@users.noreply.github.com>
2022-02-08 06:34:32 +0000

    I have completed exercise 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+  //variable declaration and initialitation
+  int mark1, mark2, sum=0;
+  float average=0;
+
+  printf("Enter marks for Subject 1: ");
+  scanf("%d", &mark1); // get user input
+  printf("Enter marks for Subject 2: ");
+  scanf("%d", &mark2); // get user input
+
+  sum = mark1 + mark2;// Calculate sub total of two marks
+  average = sum/2.0;// Calculate average mark
+
+  printf("Average mark is %.2f", average);
   
   return 0;
 }
